7dc3b286a6f7ad4ac27f3c472f69c8450e87c6ff
Yunyang Liu <ensigma96@gmail.com>
2017-11-23 13:43:56 +0800

    Add gemoji back.

---
+++ src/converter/gemoji.py
@@ -0,0 +1,19 @@
+import json
+import sys
+
+src_file_name = sys.argv[1]
+raw_data = open(src_file_name, 'r').read()
+data = json.loads(raw_data)
+
+for stuff in data:
+  if 'emoji' in stuff:
+    emoji = stuff['emoji']
+    aliases = stuff['aliases']
+    tags = stuff['tags']
+
+    if (len(aliases) > 0):
+      for alias in aliases:
+        print(emoji, alias, sep='\t')
+    if (len(tags) > 0):
+      for tag in tags:
+        print(emoji, tag, sep='\t')
