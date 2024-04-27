5d27db38100208198cbd0a9786055c20973aca96
Yunyang Liu <ensigma96@gmail.com>
2024-04-27 23:46:26 +0800

    Update emojilib

--- src/converter/emojilib.py
+++ src/converter/emojilib.py
@@ -10,19 +10,7 @@ with open(src_file_name, 'r') as f:
     data = json.load(f)
 
 for key in data:
-    v = data[key]
+    emoji = key
 
-    char = v['char']
-    emojis = set([char])
-    if v['fitzpatrick_scale']:
-        for modifier in fitzpatrick_scale_modifiers:
-            emojis.add(char + zwj + modifier)
-
-    names = set([key] + v['keywords'])
-    names_nospaces = set([])
-    for item in names:
-        names_nospaces.add(item.replace(' ', '_'))
-
-    for emoji in sorted(emojis):
-        for name in sorted(names_nospaces):
-            print(emoji, name, sep='\t')
+    for name in sorted(data[key]):
+        print(emoji, name, sep='\t')
